8b878e053a937b103d2bb57d1ad64b926824dba7
吴辉 <1972871083@qq.com>
2023-03-25 17:33:25 +0800

    更新 Puppeteer demo 脚本

--- scripts/loadpage.js
+++ scripts/loadpage.js
@@ -1,20 +1,29 @@
+/**
+ * demo loadpage.js for https://www.cxtvlive.com/live-tv/canal-once
+ * 返回 ["http://xxxx.com/xxx/xxx.m3u8","http://xxxx.com/yyy/yyy.m3u8"] 格式数据
+ */
 const puppeteer = require('puppeteer');
-var address = process.argv[2];
+var url = process.argv[2];
 
 (async () => {
   const browser = await puppeteer.launch( {args: ["--no-sandbox"]});
   const page = await browser.newPage();
-  await page.goto(address, {waitUntil: 'networkidle2'});
-  
-  const m3u8 = await page.evaluate(() => {
-    var flash =  document.getElementsByTagName("embed")[0]
-    if (flash){
-        var flashvars = flash.getAttribute("flashvars");
-        return unescape(flashvars.substr(5));
-    }else{
-        return "";
+  await page.goto(url, {waitUntil: 'networkidle2'});
+
+  const m3u8_list = await page.evaluate(() => {
+    var m3u8_list =[];
+    var videos = document.getElementsByTagName("video")
+    if (videos){
+        for (i = 0; i < videos.length; i++) { 
+            var source = videos[i].getElementsByTagName("source")
+            if (source){
+                var url = source[0].getAttribute("src")
+                m3u8_list.push(url)
+            }
+         }
     }
+    return m3u8_list;
   });
-  console.log("url:"+m3u8);
+  console.log(m3u8_list);
   await browser.close();
 })();
